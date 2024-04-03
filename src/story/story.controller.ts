2f0ffd347ed294bef9e9d48dfbed4bfd2506ad61
antisdun <112616257+antisdun@users.noreply.github.com>
2024-04-03 18:14:04 +0900

    feat: swagger 적용

--- src/story/story.controller.ts
+++ src/story/story.controller.ts
@@ -2,17 +2,25 @@ import { Controller, Post, Body, Get } from '@nestjs/common';
 import { StoryService } from './story.service';
 import { CreateStoryRequestDto } from './dto/create-story-request.dto';
 import { PaginationDto } from 'src/page/pagination.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('스토리 Api')
 @Controller('story')
 export class StoryController {
   constructor(private readonly storyService: StoryService) {}
 
   @Post()
+  @ApiOperation({ summary: '스토리 생성' })
+  @ApiResponse({ status: 201, description: '스토리가 생성되었습니다.' })
+  @ApiResponse({ status: 400, description: '스토리 생성에 실패  하였습니다.' })
   async createStory(@Body() dto: CreateStoryRequestDto) {
     return await this.storyService.createStory(dto);
   }
 
   @Get()
+  @ApiOperation({ summary: '스토리 조회' })
+  @ApiResponse({ status: 200, description: '스토리를 조회합니다.' })
+  @ApiResponse({ status: 400, description: '스토리 조회에 실패하였습니다.' })
   async getStory(@Body() dto: PaginationDto) {
     return await this.storyService.getStroy(dto);
   }

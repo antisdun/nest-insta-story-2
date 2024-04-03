c1d38666ba82b952b7c0a7a823facf6af81aef4d
antisdun <112616257+antisdun@users.noreply.github.com>
2024-04-03 18:36:59 +0900

    feat: 파이프라인 설정

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initializeTransactionalContext } from 'typeorm-transactional';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   initializeTransactionalContext();
@@ -8,6 +10,29 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // TODO: 프로그램 구현
   await app.listen(process.env.PORT || 8000);
+
+  // 파이프라인 설정
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Swagger 적용
+  const config = new DocumentBuilder()
+    .setTitle('insta-story Api')
+    .setDescription('어쩌다 nest 2주차')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, swaggerOptions);
+  SwaggerModule.setup('api', app, document);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
--- src/page/pagination.dto.ts
+++ src/page/pagination.dto.ts
@@ -1,5 +1,9 @@
+import { Type } from 'class-transformer';
+
 export class PaginationDto {
+  @Type(() => Number)
   page: number = 1;
 
+  @Type(() => Number)
   limit: number = 10;
 }

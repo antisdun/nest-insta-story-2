0c3ce1632ac756b6a5846de292a15c909144c660
antisdun <112616257+antisdun@users.noreply.github.com>
2024-04-03 16:09:38 +0900

    feat: 모듈 설정

--- src/story/story.module.ts
+++ src/story/story.module.ts
@@ -1,8 +1,11 @@
+import { Story } from './entities/story.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { StoryService } from './story.service';
 import { StoryController } from './story.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Story])],
   controllers: [StoryController],
   providers: [StoryService],
 })
